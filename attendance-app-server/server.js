2e8f96ffc5ad962307efb6da55404820c4a6408b
FerdinandYL <yusakferdinand@gmail.com>
2023-12-07 16:59:36 +0700

    Make basic server.js

--- attendance-app-server/server.js
+++ attendance-app-server/server.js
@@ -4,3 +4,13 @@ import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import cors from 'cors';
 
+const PORT = 3001;
+
+const app = express();
+
+app.use(morgan);
+app.use(cors);
+
+app.listen(process.env.PORT, () => {
+    console.log(`App listen to Port : ${PORT}`)
+})
